3cbd0138b32286128db23a0399b65e47292545c4
Victor-El <victorelezua@gmail.com>
2020-06-19 15:56:13 +0100

    restrict multiple api calls in a day

--- src/service/NewsService.js
+++ src/service/NewsService.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-import {GNEWS_API_KEY, LOCAL_STORAGE_NAME_FOR_NEWS_API, REQUEST_QUERY} from "./constants";
+import {
+    GNEWS_API_KEY,
+    LOCAL_STORAGE_NAME_FOR_CACHE_DATE,
+    LOCAL_STORAGE_NAME_FOR_NEWS_API,
+    REQUEST_QUERY
+} from "./constants";
 /*
 * Commented out code in this file were code used for news api, currently we're using gnews api
 * */
@@ -14,7 +19,11 @@ import {GNEWS_API_KEY, LOCAL_STORAGE_NAME_FOR_NEWS_API, REQUEST_QUERY} from "./c
 //     return `${year}-${month}-${day}`;
 // };
 
+// This tries as much as possible to return saved value else returns null and shows warning alert to the user
 const getAllNews = async () => {
+    const cacheDate = localStorage.getItem(LOCAL_STORAGE_NAME_FOR_CACHE_DATE) ? new Date(JSON.parse(localStorage.getItem(LOCAL_STORAGE_NAME_FOR_CACHE_DATE))) : new Date();
+    const currentDate = new Date();
+
     //const date = getDateFromAMonthAgo();
     const axiosInstance = axios.create({
         baseURL: `https://gnews.io/api/v3/`,
@@ -23,16 +32,31 @@ const getAllNews = async () => {
         // headers: {'Authorization': `Bearer ${process.env.REACT_APP_NEWS_API_KEY}`}
     });
 
-    return axiosInstance.get(`search?q=${REQUEST_QUERY}&image=required&token=${GNEWS_API_KEY}`).then((res) => {
-        if (res.data != null && res.status === 200) {
-            localStorage.setItem(LOCAL_STORAGE_NAME_FOR_NEWS_API, JSON.stringify(res.data));
-            return res.data;
-        } else {
-            return null;
-        }
-    }).catch(() => {
-        return localStorage.getItem(LOCAL_STORAGE_NAME_FOR_NEWS_API) ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_NAME_FOR_NEWS_API)) : null;
-    });
+    // Check if cache time is 24hrs or more less than current time before sending request
+    if (currentDate - cacheDate >= 60 * 60 * 24) {
+        return axiosInstance.get(`search?q=${REQUEST_QUERY}&image=required&token=${GNEWS_API_KEY}`).then((res) => {
+            if (res.data != null && res.status === 200) {
+                localStorage.setItem(LOCAL_STORAGE_NAME_FOR_NEWS_API, JSON.stringify(res.data));
+                localStorage.setItem(LOCAL_STORAGE_NAME_FOR_CACHE_DATE, JSON.stringify(new Date()));
+                return res.data;
+            } else {
+                if (!localStorage.getItem(LOCAL_STORAGE_NAME_FOR_NEWS_API)) {
+                    alert("Server Error \n Try again tomorrow");
+                    return null;
+                }
+
+                return JSON.parse(localStorage.getItem(LOCAL_STORAGE_NAME_FOR_NEWS_API));
+            }
+        }).catch(() => {
+            if (!localStorage.getItem(LOCAL_STORAGE_NAME_FOR_NEWS_API)) {
+                alert("Server Error \n Try again tomorrow");
+                return null;
+            }
+            return JSON.parse(localStorage.getItem(LOCAL_STORAGE_NAME_FOR_NEWS_API));
+        });
+    } else {
+        return JSON.parse(localStorage.getItem(LOCAL_STORAGE_NAME_FOR_NEWS_API));
+    }
 
 };
 
--- src/service/constants.js
+++ src/service/constants.js
@@ -1,3 +1,4 @@
 export const REQUEST_QUERY = `Coronavirus | Covid-19`;
 export const LOCAL_STORAGE_NAME_FOR_NEWS_API = "news-api";
 export const GNEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+export const LOCAL_STORAGE_NAME_FOR_CACHE_DATE = "cache-date";
